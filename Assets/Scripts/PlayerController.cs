5bc271a99f40523d8da728edd9491640fffc078c
Cole <colewatts26@gmail.com>
2020-08-14 13:43:34 -0500

    Playercontroller fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,12 +9,22 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
 
     public int jumpForce;
+    public Transform groundPoint;
+
+    public LayerMask groundLayer;
+
+    public bool grounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
+    //Update method used for phydics in our game, becaues it happens fixed amount of times
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapPoint(groundPoint.position,groundLayer);
+    }//end Fixed Update
 
     // Update is called once per frame
     void Update()
@@ -23,6 +33,7 @@ public class PlayerController : MonoBehaviour
         {
             anim.SetTrigger("Jump");
             rb.AddForce(Vector2.up * jumpForce);
+            
 
 
         }//end if
